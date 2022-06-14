18c7a14c4bddaf7bd3c791e0d4a8122b7e32a791
mellotonio <antoniombabo@uol.com.br>
2022-06-13 23:37:18 -0400

    refact: main.go

--- main.go
+++ main.go
@@ -1,11 +1,60 @@
 package main
 
 import (
-	"os"
+	"fmt"
 
-	repl "github.com/ZooeyLang/REPL"
+	evaluator "github.com/ZooeyLang/Evaluator"
+	lexer "github.com/ZooeyLang/Lexer"
+	object "github.com/ZooeyLang/Object"
+	parser "github.com/ZooeyLang/Parser"
 )
 
 func main() {
-	repl.Start(os.Stdin, os.Stdout)
+
+	env := object.NewEnvironment()
+	l := lexer.New(`
+	fn passouDeAno(nota){
+		if nota > 7 {
+			return true
+		}else {
+			return false
+		}
+	}
+
+	owo notaUm :=: 7; 
+	owo notaDois :=: 8; 
+	owo notaTres :=: 5; 
+	owo notaQuatro :=: 4; 
+
+	owo media :=: (notaUm + notaDois + notaTres + notaQuatro) / 4;
+	show(media)
+	show(passouDeAno(media))
+
+	for(owo i :=: 0; i <= 10; i++){
+		media :=: media + 10
+	}
+
+	show(media)
+
+	owo testeEscopo :=: "eu valho porcaria ninhuma..."
+
+	fn valorizador(){
+	 testeEscopo :=: "finalmente reconheceram meu valor..."
+	 show(testeEscopo);
+	}
+	valorizador()
+	show(testeEscopo)
+	`)
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+
+	if len(p.Errors()) != 0 {
+		fmt.Println(p.Errors())
+	}
+
+	evaluated := evaluator.Eval(program, env)
+	if evaluated != nil {
+		fmt.Println(evaluated)
+	}
 }
